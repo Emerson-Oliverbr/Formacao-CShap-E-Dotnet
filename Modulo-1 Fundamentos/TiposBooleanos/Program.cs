af084bd91bdc20b6a1b45778e1b1709513daa80a
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-03-05 17:33:45 -0300

    Add: arquivo readme

---
+++ Modulo-1 Fundamentos/TiposBooleanos/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace TiposBooleanos;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string? usuarioCorreto = "admin";
+        string? senhaCorreta = "1234";
+
+        Console.Write("Usuário: ");
+        string? usuarioInput = Console.ReadLine();
+
+        Console.Write("Senha: ");
+        string? senhaInput = Console.ReadLine();
+        
+        bool credenciaisValidas = (usuarioInput == usuarioCorreto) && (senhaInput == senhaCorreta);
+        bool contaBloqueada = false;
+
+        if (credenciaisValidas && !contaBloqueada)
+        {
+            Console.WriteLine("Login realizado com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("Falha no login. Verifique os dados ou o status da conta.");
+        }
+    }
+}
