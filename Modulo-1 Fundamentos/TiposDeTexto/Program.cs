924e5fdcb8db5666ed6f8d9383055c9ec1017453
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-03-05 18:39:03 -0300

    Tipos de texto strings

---
+++ Modulo-1 Fundamentos/TiposDeTexto/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace TiposDeTexto;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //char
+        //string
+        Console.Clear();
+
+        char letra = 'A';
+        char numero = '1';
+        char caracter = '@';
+
+        string texto = "Hello World";
+        char primeiraLetraDoTexto = texto[0];
+        Console.WriteLine(texto);
+        Console.WriteLine(primeiraLetraDoTexto);
+
+
+        string meuNomeCompleto = "           Emerson Matos Oliveira";
+        Console.WriteLine(meuNomeCompleto);
+        string meuNomeCompletoSemEspaco = meuNomeCompleto.Trim();
+        Console.WriteLine(meuNomeCompletoSemEspaco);
+
+        //bool comecaComALetraE = meuNomeCompletoSemEspaco.StartsWith("E");
+        bool comecaComALetraE = meuNomeCompletoSemEspaco.StartsWith("e");
+        //bool comecaComALetraE = meuNomeCompletoSemEspaco.StartsWith("M");
+        Console.WriteLine(comecaComALetraE);
+
+        //bool terminaComALetraA = meuNomeCompletoSemEspaco.EndsWith("A");
+        bool terminaComALetraA = meuNomeCompletoSemEspaco.EndsWith("a");
+        //bool terminaComALetraA = meuNomeCompletoSemEspaco.EndsWith("X");
+        Console.WriteLine(terminaComALetraA);
+
+        bool existe = texto.Contains("Hello");
+        bool naoExiste = texto.Contains("hello");
+
+        Console.WriteLine(existe);
+        Console.WriteLine(naoExiste);
+
+        Console.ReadKey();
+    }
+}
+
